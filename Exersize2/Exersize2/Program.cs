c0f3422e616a69523b62b46204d81dd1dd1f8b2e
kristina.kulitsch@gmail.com <kristina.kulitsch@gmail.com>
2020-04-08 07:35:56 +0300

    Didn't work

---
+++ Exersize2/Exersize2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace YuliaExersize
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            for (int i = 1, i <= 50, i++)
+            {
+                int a = i / 3;
+                int b = i / 5;
+                if ((Convert.ToDouble(i / 3) = i / 3) & (Convert.ToDouble(i / 5) != i / 5))
+                
+                {
+                    Console.WriteLine("Tutti");
+                }
+                else if ((Convert.ToDouble(i / 5) = i / 5) & (Convert.ToDouble(i / 3) != i / 3))
+                {
+                    Console.WriteLine("Frutti");
+                }
+                else if ((Convert.ToDouble(i / 3) = i / 3) & (Convert.ToDouble(i / 5) = i / 5))
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                }
+            }
+        }
+    }
+}
