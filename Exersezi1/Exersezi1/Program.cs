323f67337e6a0071e1c233de27c995f5fa5ee31e
kristina.kulitsch@gmail.com <kristina.kulitsch@gmail.com>
2020-04-08 07:19:00 +0300

    It Works

---
+++ Exersezi1/Exersezi1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Yulia_Exersize2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, sum = 0;
+            Console.Write("Input n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The first N natural numbers are: ");
+
+            if (n <= 100)
+            {
+
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write("{0} ", i);
+                    sum += i;
+
+                }
+                Console.Write("\nThe Sum of Natural Number upto {0} terms : {1} \n", n, sum);
+            }
+
+            else
+            {
+                Console.WriteLine("Not acceptable");
+            }
+            Console.ReadKey();
+        }
+    }
+}
