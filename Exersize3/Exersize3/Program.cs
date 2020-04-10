4d62398925ad1d3504df9405875c48858535e76c
kristina.kulitsch@gmail.com <kristina.kulitsch@gmail.com>
2020-04-10 04:08:38 +0300

    Exersize3

---
+++ Exersize3/Exersize3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exersize3
+{
+    class Program
+    {
+        //3. ввести в консоль 2 числа создать методы :
+        //сумма чисел, произведение, деление результат методов - 
+        //вывод посчитанного значения этих двух чисел в консоль
+        public static void Main(string[] args)
+        {          
+            Console.Write("Input 1st number: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input 2st number: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double d = Devision(a, b);
+            Console.Write("Result of devision by {0} and {1} is {2}", a, b, d);
+            Console.WriteLine("\nn");
+
+            double s = Sum(a, b);
+
+            Console.Write("Result of sum of {0} and {1} is {2}", a, b, s);
+            Console.WriteLine("\nn");
+            double m = Multiplication(a, b);
+            Console.Write("Result of multiplication of {0} and {1} is {2}", a, b, m);
+            Console.WriteLine("\nn");
+
+            static double Devision(double a, double b)
+            {
+                return a/b;
+            }
+
+            static double Sum(double a, double b)
+            {
+                return a +b;
+            }
+            static double Multiplication(double a, double b)
+            {
+                return a * b;
+            }
+        }
+    }
+}
+
